bf8cf75f84d150a97e6e9a52a0f3b5e05be40af7
sefimov <sefimov@licard.com>
2022-06-08 21:56:04 +0300

    add to find color

--- app.py
+++ app.py
@@ -2,6 +2,8 @@
 # API запрос для решения квадратного уравнения
 # Пример запроса: http://127.0.0.1:5000/equation?a=1&b=1&c=-6
 #
+# API запрос для угадывания цвета
+# Пример запроса: http://127.0.0.1:5000/color/99
 # email: efimooov.stanislav@gmail.com
 # ------------------------------------------------------------
 
@@ -9,6 +11,7 @@ import traceback
 from flask import Flask
 from flask_restful import Api, request
 import math
+import random
 app = Flask(__name__)
 api = Api(app)
 app.config['JSON_AS_ASCII'] = False
@@ -19,9 +22,9 @@ def get_equal(a, b, discr):
     x2 = (-b - math.sqrt(discr)) / (2 * a)
     return round(x1, 2), round(x2, 2)
 
-
+# для решения квадратного уравнения
 @app.route('/equation/', methods=['GET'])
-def user():
+def equal():
     try:
         args = request.args
         a = args.get('a')
@@ -53,10 +56,30 @@ def user():
             else:
                 x1, x2 = get_equal(a, b, discriminant)
                 return f'Корни уравнения: x1 = {x1}, x2 = {x2}'
-
     except:
         traceback.print_exc()
         return 'Такого запроса нет'
+
+
+# для угадывания цвета
+@app.route('/color/<int:num>', methods=['GET'])
+def color(num):
+    try:
+        red = random.randint(15,20)
+        green = random.randint(25,35)
+        blue = 100 - red - green
+
+        arr_red = ['Красный' for i in range(red)]
+        arr_green = ['Зеленый' for i in range(green)]
+        arr_blue = ['Синий' for i in range(blue)]
+        all_arr = arr_blue + arr_green + arr_red
+        random.shuffle(all_arr)
+
+        return f'Вы выбрали {all_arr[num]} цвет.'
+    except:
+        traceback.print_exc()
+        return 'Такого запроса нет'
+
 
 if __name__ == '__main__':
     app.run(debug=True)
